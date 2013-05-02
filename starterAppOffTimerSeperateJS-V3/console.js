72d0a359d5bd952a229ed29ca5aca701e9336180
Hubert Wong <hubertwwong@gmail.com>
2013-05-02 00:56:35 -0700

    Added a few more comments.

--- starterAppOffTimerSeperateJS-V3/console.js
+++ starterAppOffTimerSeperateJS-V3/console.js
@@ -1,6 +1,12 @@
-// a simple console file.
-// assumes that there is a text box has an id='console'
-// using a object literal. sorta like a class.
+/*
+	a simple console function that writes to a text box.
+	assumes that there is a text box has an id='console'
+	using a object literal. sorta like a class.
+	
+	there are 2 functions. println has a newline added to the end
+	print
+	println
+*/
 var con = function () {
 	var console = document.getElementById('console');
 	console.value = "";
